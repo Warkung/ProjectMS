8e0c2574c2817e091d94396a7829e881315b9cae
Warkung <warkung@gmail.com>
2025-05-21 08:06:16 +0700

    commit

--- server/server.js
+++ server/server.js
@@ -1,12 +1,19 @@
-const express = require('express');
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+
+require("dotenv").config();
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT;
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
